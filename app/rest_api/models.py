5f3cba654fbdfd4c6d897e3ad84ee9e23e024527
Tomasz <korniszuk.tomasz@gmail.com>
2023-03-02 12:01:14 +0100

    Adding ability to create expiring links

--- app/rest_api/models.py
+++ app/rest_api/models.py
@@ -52,7 +52,6 @@ class Image(models.Model):
         size = (width, int(height))
         img.thumbnail(size)
 
-        thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
         thumbnail = File(thumb_io, name=image.name)
 
